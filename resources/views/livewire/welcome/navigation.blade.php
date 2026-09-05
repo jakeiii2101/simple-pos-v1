f88683083d6d6c4dcc9e5cbd3d64033b4b9f63ed
Jk Hambala <jkhambala00@gmail.com>
2026-09-05 22:18:59 +0800

    Use relative URLs in welcome navigation

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,14 +1,14 @@
 <nav class="-mx-3 flex flex-1 justify-end">
     @auth
         <a
-            href="{{ url('/dashboard') }}"
+            href="/dashboard"
             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
         >
             Dashboard
         </a>
     @else
         <a
-            href="{{ route('login') }}"
+            href="{{ route('login', [], false) }}"
             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
         >
             Log in
@@ -16,7 +16,7 @@
 
         @if (Route::has('register'))
             <a
-                href="{{ route('register') }}"
+                href="{{ route('register', [], false) }}"
                 class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
             >
                 Register
