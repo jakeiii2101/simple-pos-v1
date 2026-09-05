00b4b5ccc382218c9720a8d04c1241b88d1ff2e7
Jk Hambala <jkhambala00@gmail.com>
2026-09-05 21:21:14 +0800

    Protect application routes with active account middleware

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 
 Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth', 'active', 'verified'])
     ->name('dashboard');
 
 Route::view('profile', 'profile')
-    ->middleware(['auth'])
+    ->middleware(['auth', 'active'])
     ->name('profile');
 
 require __DIR__.'/auth.php';
