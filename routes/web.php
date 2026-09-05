313b2dfd9d91f4553a9492f5e1e30f3158984e8d
Jk Hambala <jkhambala00@gmail.com>
2026-09-05 22:37:44 +0800

    Add inventory route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Categories\CategoryList;
+use App\Livewire\Inventory\InventoryList;
 use App\Livewire\Products\ProductList;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,7 @@ Route::view('profile', 'profile')
 Route::middleware(['auth', 'active', 'role:admin'])->group(function () {
     Route::get('categories', CategoryList::class)->name('categories');
     Route::get('products', ProductList::class)->name('products');
+    Route::get('inventory', InventoryList::class)->name('inventory');
 });
 
 require __DIR__.'/auth.php';
