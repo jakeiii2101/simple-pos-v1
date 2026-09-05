2a0967b1ab699fb68f1d4bd53e2d4e0efa249733
Jk Hambala <jkhambala00@gmail.com>
2026-09-05 23:58:12 +0800

    Add PWA metadata to guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -2,10 +2,19 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="theme-color" content="#111827">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="default">
+        <meta name="apple-mobile-web-app-title" content="Simple POS">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Simple POS') }}</title>
+
+        <link rel="manifest" href="/manifest.webmanifest">
+        <link rel="icon" href="/icons/simple-pos-icon.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/icons/simple-pos-icon.svg">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
