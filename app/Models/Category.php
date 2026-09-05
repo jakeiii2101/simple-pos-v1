8a6c98cf1eea5cc4d15312ab89c37383a77c80e1
Jk Hambala <jkhambala00@gmail.com>
2026-09-05 22:25:07 +0800

    Add products relationship to categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['name', 'description', 'status'])]
 class Category extends Model
@@ -14,6 +15,11 @@ class Category extends Model
     public const STATUS_ACTIVE = 'active';
 
     public const STATUS_INACTIVE = 'inactive';
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 
     public function isActive(): bool
     {
